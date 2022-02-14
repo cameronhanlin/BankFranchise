3f68bb03bb020e86e9aab6e54d1d20143cfda368
Cameron <cameronhanlin@Camerons-MacBook-Pro.local>
2022-02-14 13:45:02 -0700

    a test update comment

--- src/com/company/BankFranchise.java
+++ src/com/company/BankFranchise.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class BankFranchise {
 
     public static void main(String[] args) {
-
+// this is a test comment
     Scanner scan = new Scanner(System.in);
     Bank bank1 = new Bank();
     Bank bank2 = new Bank();
